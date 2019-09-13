e00153a0e2db7d83aba8f6c9d2db7d10e4317a25
Thomas Wilder <tom16wilder@googlemail.com>
2019-09-13 11:08:34 +0100

    new file:   abscurl.m

---
+++ abscurl.m
@@ -0,0 +1,16 @@
+function [Curl] = abscurl(y, alpha, beta)
+
+% Curl of wind stress using resting ocean approximation
+
+% Parameter Values
+rhoa = (10^9)*1.225; % kg/km^3
+Cd = 1.5e-3; % Kara 2007
+
+for i = 1:length(y)
+    for j = 1:length(y)      
+        Curl(i,j) = -(10^-6)*rhoa*Cd*alpha*( ((alpha*y(i,j)+beta)^2)/(abs(alpha*y(i,j)+beta))+abs(alpha*y(i,j)+beta));
+    end
+end
+
+end
+
